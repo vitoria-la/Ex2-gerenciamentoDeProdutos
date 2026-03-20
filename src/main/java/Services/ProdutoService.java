9c61e99b5cd96bc66bc541e26755a58ea580bfeb
vitoria-la <laura.v.fabiani@gmail.com>
2026-03-20 11:40:39 -0300

    Adicionando o Service

---
+++ src/main/java/Services/ProdutoService.java
@@ -0,0 +1,31 @@
+package Services;
+
+import com.exercicios.gerenciamentoDeProdutos.Models.ProdutoModel;
+import com.exercicios.gerenciamentoDeProdutos.Repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    public ProdutoModel criarProduto(ProdutoModel produtoModel) {
+        return produtoRepository.save(produtoModel);
+    }
+    public List<ProdutoModel> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<ProdutoModel> buscarProduto(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public void deletarProduto(Long id) {
+        produtoRepository.deleteById(id);
+    }
+}
